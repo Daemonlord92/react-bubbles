d587b45a7df6d86bf604b648c94e1a62574a7b8b
Matthew Martin <zhintateo19924559@gmail.com>
2020-06-12 20:37:26 -0500

    Added Reactstrap, And got login working

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { Container, Col } from 'reactstrap';
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from "./components/BubblePage";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Route exact path="/" component={Login} />
+      <Container className="App">
+          <Col>
+              <Switch>
+                  <PrivateRoute extact path="/bubbles" component={BubblePage}/>
+                  <Route exact path="/" component={Login} />
+              </Switch>
+          </Col>
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
-      </div>
+      </Container>
     </Router>
   );
 }
